5abd65dff9c12b74682279c83fd9e416d4ad400c
felquis <ofelquis@hotmail.com>
2013-03-08 17:31:05 -0300

    primeiro treste

---
+++ gruntfile.js
@@ -0,0 +1,30 @@
+﻿module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    // Meta informations
+    pkg: '<json:package.json>',
+    meta: {
+      banner: '/*\n' +
+        ' *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n' +
+        ' *  <%= pkg.description %>\n' +
+        ' *  <%= pkg.homepage %>\n\n' +
+        ' *  Copyright (c) <%= grunt.template.today("yyyy") %>\n' +
+        ' *  MIT License\n' +
+        ' */'
+    },
+
+    // Lint definitions
+    lint: {
+      files: ['js/main.js']
+    },
+    jshint: {
+      options: {
+        multistr: true
+      }
+    }
+  });
+
+  grunt.registerTask('default', 'lint');
+  grunt.registerTask('travis', 'lint');
+};
